ad5827068423fb17fa1ae26110888e388608f687
szabogabriel <szabo.gabriel@gmail.com>
2023-01-30 08:27:40 +0100

    Extract NotebookService from NotebookPopupMenu class

--- Notes/src/com/notes/gui/menu/NotebookPopupMenu.java
+++ Notes/src/com/notes/gui/menu/NotebookPopupMenu.java
@@ -1,22 +1,14 @@
 package com.notes.gui.menu;
 
 import java.awt.Component;
-import java.awt.event.ActionEvent;
-import java.io.File;
 
-import javax.swing.JFileChooser;
 import javax.swing.JList;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
-import com.notes.gui.model.GuiAppModel;
-import com.notes.gui.model.GuiNoteModel;
 import com.notes.gui.model.GuiNotebookModel;
-import com.notes.gui.model.GuiTopicModel;
-import com.notes.model.NoteModel;
-import com.notes.model.NotebookModel;
-import com.notes.model.TopicModel;
 import com.notes.service.ContextHolderService;
+import com.notes.service.gui.NotebookService;
 
 public class NotebookPopupMenu extends JPopupMenu {
 
@@ -27,70 +19,30 @@ public class NotebookPopupMenu extends JPopupMenu {
 	private JMenuItem renameNotebookItem = new JMenuItem("Rename notebook");
 	private JMenuItem removeNotebookItem = new JMenuItem("Delete notebook");
 
-	private ContextHolderService contextHolder;
 	private JList<GuiNotebookModel> parent;
+	
+	private NotebookService notebookService;
 	
 	@SuppressWarnings("unchecked")
 	public NotebookPopupMenu(Component parent, ContextHolderService contextHolder) {
+		notebookService = new NotebookService((JList<GuiNotebookModel>)parent, contextHolder);
 		this.parent = (JList<GuiNotebookModel>)parent;
-		this.contextHolder = contextHolder;
 		
 		add(openNotebookItem);
-		openNotebookItem.addActionListener(this::openNotebookListener);
+		openNotebookItem.addActionListener(notebookService::openNotebookListener);
 		
 		add(createNotebookItem);
-		createNotebookItem.addActionListener(this::createNotebookListener);
+		createNotebookItem.addActionListener(notebookService::createNotebookListener);
 		
 		if (this.parent.getSelectedValue() != null) {
 			add(renameNotebookItem);
-			renameNotebookItem.addActionListener(this::renameNotebookListener);
+			renameNotebookItem.addActionListener(notebookService::renameNotebookListener);
 			
 			if (this.parent.getModel().getSize() > 1) {
 				add(removeNotebookItem);
-				removeNotebookItem.addActionListener(this::removeNotebookItem);
+				removeNotebookItem.addActionListener(notebookService::removeNotebookItem);
 			}
 		}
 	}
 	
-	private void openNotebookListener(ActionEvent e) {
-		JFileChooser fc = new JFileChooser();
-		int result = fc.showOpenDialog(parent);
-		if (result == JFileChooser.APPROVE_OPTION) {
-			File selected = fc.getSelectedFile();
-			if (selected.isFile()) {
-				contextHolder.loadNotebook(selected);
-			}
-		}
-	}
-	
-	private void createNotebookListener(ActionEvent e) {
-		JFileChooser fc = new JFileChooser();
-		int result = fc.showSaveDialog(parent);
-		if (result == JFileChooser.APPROVE_OPTION) {
-			File newFile = fc.getSelectedFile();
-			if (!newFile.getName().endsWith("xnotes")) {
-				newFile = new File(newFile.getAbsolutePath() + ".xnotes");
-			}
-			String modelName = newFile.getName().split("\\\\.")[0];
-			GuiNotebookModel newNotebook = new GuiNotebookModel(new NotebookModel(modelName));
-			GuiTopicModel newTopic = new GuiTopicModel(new TopicModel("Default"));
-			GuiNoteModel noteModel = new GuiNoteModel(new NoteModel("Default"));
-			
-			newTopic.addNote(noteModel);
-			newNotebook.addGuiTopicModel(newTopic);
-			
-			GuiAppModel appModel = (GuiAppModel)parent.getModel();
-			appModel.addNotebook(newNotebook, newFile);
-			
-			parent.setSelectedIndex(0);
-		}
-	}
-	
-	private void renameNotebookListener(ActionEvent e) {
-		//TODO
-	}
-	
-	private void removeNotebookItem(ActionEvent e) {
-		//TODO
-	}
 }
---
+++ Notes/src/com/notes/service/gui/NotebookService.java
@@ -0,0 +1,70 @@
+package com.notes.service.gui;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JList;
+
+import com.notes.gui.model.GuiAppModel;
+import com.notes.gui.model.GuiNoteModel;
+import com.notes.gui.model.GuiNotebookModel;
+import com.notes.gui.model.GuiTopicModel;
+import com.notes.model.NoteModel;
+import com.notes.model.NotebookModel;
+import com.notes.model.TopicModel;
+import com.notes.service.ContextHolderService;
+
+public class NotebookService {
+	
+	private ContextHolderService contextHolder;
+	private JList<GuiNotebookModel> parent;
+	
+	public NotebookService(JList<GuiNotebookModel> parent, ContextHolderService contextHolder) {
+		this.parent = parent;
+		this.contextHolder = contextHolder;
+	}
+	
+	public void openNotebookListener(ActionEvent e) {
+		JFileChooser fc = new JFileChooser();
+		int result = fc.showOpenDialog(parent);
+		if (result == JFileChooser.APPROVE_OPTION) {
+			File selected = fc.getSelectedFile();
+			if (selected.isFile()) {
+				contextHolder.loadNotebook(selected);
+			}
+		}
+	}
+	
+	public void createNotebookListener(ActionEvent e) {
+		JFileChooser fc = new JFileChooser();
+		int result = fc.showSaveDialog(parent);
+		if (result == JFileChooser.APPROVE_OPTION) {
+			File newFile = fc.getSelectedFile();
+			if (!newFile.getName().endsWith("xnotes")) {
+				newFile = new File(newFile.getAbsolutePath() + ".xnotes");
+			}
+			String modelName = newFile.getName().split("\\\\.")[0];
+			GuiNotebookModel newNotebook = new GuiNotebookModel(new NotebookModel(modelName));
+			GuiTopicModel newTopic = new GuiTopicModel(new TopicModel("Default"));
+			GuiNoteModel noteModel = new GuiNoteModel(new NoteModel("Default"));
+			
+			newTopic.addNote(noteModel);
+			newNotebook.addGuiTopicModel(newTopic);
+			
+			GuiAppModel appModel = (GuiAppModel)parent.getModel();
+			appModel.addNotebook(newNotebook, newFile);
+			
+			parent.setSelectedIndex(0);
+		}
+	}
+	
+	public void renameNotebookListener(ActionEvent e) {
+		//TODO
+	}
+	
+	public void removeNotebookItem(ActionEvent e) {
+		//TODO
+	}
+
+}
